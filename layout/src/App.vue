fd4a82f65dfcf11a99aaa4ca1917880538043a46
2290774104 <2290774104@qq.com>
2024-06-12 15:25:04 +0800

    feat: 基座应用项目改造

--- layout/src/App.vue
+++ layout/src/App.vue
@@ -1,7 +1,76 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { ref, type CSSProperties, VueElement, reactive } from 'vue'
+import type { ItemType, MenuProps } from 'ant-design-vue'
+import router from './router'
+
+const layoutStyle: CSSProperties = {
+  height: '100%'
+}
+
+const headerStyle: CSSProperties = {
+  textAlign: 'center',
+  lineHeight: '60px',
+  color: '#fff'
+}
+
+const contentStyle: CSSProperties = {}
+
+const siderStyle: CSSProperties = {}
+
+const selectedKeys = ref<string[]>(['Vue2'])
+const openKeys = ref<string[]>(['1'])
+
+function getItem(
+  label: VueElement | string,
+  key: string,
+  icon?: any,
+  children?: ItemType[],
+  type?: 'group'
+): ItemType {
+  return {
+    key,
+    icon,
+    children,
+    label,
+    type
+  } as ItemType
+}
+
+const items: ItemType[] = reactive([
+  getItem('子项目', '1', null, [
+    getItem('Vue', 'vue', null, [getItem('Vue2', 'Vue2'), getItem('Vue3', 'Vue3')], 'group')
+  ])
+])
+
+const routers = {
+  Vue2: '/app1',
+  Vue3: '/app2'
+}
+
+const handleClick: MenuProps['onClick'] = (e) => {
+  console.log('click', e)
+  router.push({
+    path: routers[e.key]
+  })
+}
+</script>
 
 <template>
-  <div id="app"></div>
+  <a-layout :style="layoutStyle">
+    <a-layout-sider :style="siderStyle">
+      <a-menu
+        v-model:openKeys="openKeys"
+        v-model:selectedKeys="selectedKeys"
+        mode="inline"
+        :items="items"
+        @click="handleClick"
+      ></a-menu
+    ></a-layout-sider>
+    <a-layout>
+      <a-layout-header :style="headerStyle">子项目</a-layout-header>
+      <a-layout-content :style="contentStyle" id="microApp"></a-layout-content>
+    </a-layout>
+  </a-layout>
 </template>
 
-<style></style>
+<style lang="scss"></style>
--- layout/src/main.ts
+++ layout/src/main.ts
@@ -6,9 +6,11 @@ import { createPinia } from 'pinia'
 import App from './App.vue'
 import router from './router'
 
+import Antd from 'ant-design-vue'
+
 import { registerApplication, start } from 'single-spa'
 
-const creatScript = (url: string) => {
+const createScript = (url: string) => {
   return new Promise((resolve, reject) => {
     const script = document.createElement('script')
     script.src = url
@@ -19,9 +21,36 @@ const creatScript = (url: string) => {
   })
 }
 
+function loadApp(url: string, globalVar: string) {
+  // 支持远程加载子应用
+  return async () => {
+    await createScript(url + '/js/chunk-vendors.js')
+    await createScript(url + '/js/app.js')
+    // 这里的return很重要，需要从这个全局对象中拿到子应用暴露出来的生命周期函数
+    return window[globalVar]
+  }
+}
+
+const apps = [
+  {
+    name: 'app1',
+    app: loadApp('http://localhost:8081', 'app1'),
+    activeWhen: (location: any) => location.pathname.startsWith('/app1'),
+    customProps: {}
+  }
+]
+
+// 注册子应用
+for (let i = apps.length - 1; i >= 0; i--) {
+  registerApplication(apps[i])
+}
+
 const app = createApp(App)
 
 app.use(createPinia())
 app.use(router)
+app.use(Antd)
 
 app.mount('#app')
+
+start()
